785ba26a725a30b0fd4f87239efb5c7784cb2863
a951087423 <v0973570232@gmail.com>
2022-08-16 15:06:02 +0800

    update

--- cmds/指令.py
+++ cmds/指令.py
@@ -16,23 +16,37 @@ class 指令(Cog_Extension):
     async def reload(self, ctx, extension):
         self.bot.reload_extension(f"cmds.{extension}")
         await ctx.send(f"重新載入 >>{extension}<< 完成！！")
+
+
     @commands.command()
     async def userinfo(self, ctx, user:discord.Member=None):
-
+        
+        roles = []
         if user==None:
             user=ctx.author
+        for role in user.roles:
+            roles.append(str(role.mention))
 
-        embed=discord.Embed(title=f"UserInfo - {user}", description="‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ", color=0x2f56ee, timestamp = datetime.datetime.utcnow())
+        roles.reverse()
+
+        embed=discord.Embed(title=f":boy: 使用者資訊 - {user}", description="‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ", color=0x2f56ee, timestamp = datetime.datetime.utcnow())
         embed.set_thumbnail(url=user.avatar_url),
-        embed.set_author(name=ctx.message.author.name, icon_url=user.avatar_url)
-        embed.add_field(name="Name", value=user.display_name, inline=True)
-        embed.add_field(name="ID", value=user.id, inline=True)
-        embed.add_field(name="‎", value="-", inline=False)
-        embed.add_field(name="Created Discord Account", value=user.created_at, inline=False)
-        embed.add_field(name="Joined this server", value=user.joined_at, inline=False)
-        embed.add_field(name="‎", value="-", inline=False)
+        embed.set_author(name="[開發中] 西瓜狗狗", icon_url="https://i.imgur.com/MLVO5sl.jpg")
+        embed.add_field(name=":mag_right: 名稱", value=user.display_name, inline=True)
+        embed.add_field(name=":id: Discord ID", value=user.id, inline=True)
+        embed.add_field(name="‎", value="‎", inline=False)
+        embed.add_field(name=":calendar_spiral: 創建Discord帳號於", value=user.created_at, inline=False)
+        embed.add_field(name=":calendar_spiral: 加入此伺服器於", value=user.joined_at, inline=False)
+        embed.add_field(name="‎", value="‎", inline=False)
+        if len(str(" | ".join([x.mention for x in user.roles]))) > 1024:
+            embed.add_field(name=f":crown: 身份組 [{len(user.roles)}]", value="滿出來了！", inline=False)
+        else:
+            embed.add_field(name=f":crown: 身份組 [{len(user.roles)}]", value=" | ".join(roles), inline=False)
+
         embed.set_footer(text=f" {ctx.author}", icon_url = ctx.author.avatar_url)
         await ctx.send(embed=embed)
+
+
     @commands.command()
     async def 聊天(self, ctx):
         embed=discord.Embed(title="列表：", description="註：以下為與機器人聊天指令 不須加入前綴  ~  ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ", color=0x58ef2e, timestamp = datetime.datetime.utcnow())
